15cf422727c31187a1e9850c702c0c99912e3355
Tuan <anhtuan15199@gmail.com>
2021-03-01 11:10:55 +0700

    create customer-service and apis for manage customer

--- customer-service/src/main/java/vn/viettel/customer/CustomerServiceApplication.java
+++ customer-service/src/main/java/vn/viettel/customer/CustomerServiceApplication.java
@@ -2,8 +2,20 @@ package vn.viettel.customer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableEurekaClient
+@EnableFeignClients
+@EnableJpaAuditing
+@EnableAsync
+@EntityScan("vn.viettel.core.db.entity")
+@ComponentScan(basePackages = {"vn.viettel.customer", "vn.viettel.core.handler", "vn.viettel.core.security", "vn.viettel.core.service"})
 public class CustomerServiceApplication {
 
 	public static void main(String[] args) {
