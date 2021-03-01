a6bf9454a32feeeb329665542dca7265398012ef
Tuan <anhtuan15199@gmail.com>
2021-03-01 11:12:00 +0700

    create promotion-service

---
+++ promotion-service/src/main/java/vn/viettel/promotion/PromotionServiceApplication.java
@@ -0,0 +1,13 @@
+package vn.viettel.promotion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PromotionServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PromotionServiceApplication.class, args);
+	}
+
+}
