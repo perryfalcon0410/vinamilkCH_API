6626088865390581561720926c5d562a5db6ef24
Vang Huynh <vanghm@imt-soft.com>
2021-03-05 13:11:11 +0700

    add file build

---
+++ scripts/build.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+cd ../core-entity
+mvn clean
+mvn install -Dmaven.test.skip=true
+cd ..
+
+cd core-util
+mvn clean
+mvn install -Dmaven.test.skip=true
+cd ..
+
+cd core-processor
+mvn install -Dmaven.test.skip=true
+cd ..
+
+cd discovery-server
+mvn clean
+mvn install -Dmaven.test.skip=true
+cd ..
+
+cd api-gateway
+mvn clean
+mvn install -Dmaven.test.skip=true
+cd ..
+
+cd authorization-service
+mvn clean
+mvn install -Dmaven.test.skip=true
+cd ..
+
+cd common-service
+mvn clean
+mvn install -Dmaven.test.skip=true
+cd ..
+
+cd customer-service
+mvn clean
+mvn install -Dmaven.test.skip=true
+cd ..
+
+cd promotion-service
+mvn clean
+mvn install -Dmaven.test.skip=true
+cd ..
+
+cd report-service
+mvn clean
+mvn install -Dmaven.test.skip=true
+cd ..
+
+cd sale-service
+mvn clean
+mvn install -Dmaven.test.skip=true
+cd ..
+
+cd setting-service
+mvn clean
+mvn install -Dmaven.test.skip=true
+cd ..
+
