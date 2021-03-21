0fd585807802e13f329ff181946f154cfe8c3d4c
Tuan Bui <anhtuan15199@gmail.com>
2021-03-21 17:02:28 +0700

    adjust api search product

--- sale-service/src/main/java/vn/viettel/saleservice/controller/SearchController.java
+++ sale-service/src/main/java/vn/viettel/saleservice/controller/SearchController.java
@@ -1,44 +1,54 @@
 package vn.viettel.saleservice.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.*;
 import vn.viettel.core.db.entity.Product;
 import vn.viettel.core.db.entity.ProductType;
 import vn.viettel.core.db.entity.ReceiptOnline;
 import vn.viettel.core.messaging.Response;
-import vn.viettel.saleservice.service.SearchService;
-import vn.viettel.saleservice.service.dto.ReceiptSearch;
+import vn.viettel.saleservice.service.SearchProductService;
+import vn.viettel.saleservice.service.SearchReceiptOnlineService;
 
-import java.util.List;
+import java.util.Date;
 
 @RestController
 @RequestMapping("/api/sale")
 public class SearchController {
     @Autowired
-    SearchService searchService;
+    SearchReceiptOnlineService searchReceiptOnlineService;
+
+    @Autowired
+    SearchProductService searchProductService;
 
     @GetMapping("/search/product-type/all")
-    public Response<List<ProductType>> getALlProductType() {
-        return searchService.getAllProductType();
+    public Response<Page<ProductType>> getALlProductType(Pageable pageable) {
+        return searchProductService.getAllProductType(pageable);
     }
 
     @GetMapping("/search/get-product-by-type/{proTypeId}")
-    public Response<List<Product>> getProductByProductTypeId(@PathVariable long proTypeId) {
-        return searchService.getProductByProductTypeId(proTypeId);
+    public Response<Page<Product>> getProductByProductTypeId(@PathVariable long proTypeId, Pageable pageable) {
+        return searchProductService.getProductByProductTypeId(proTypeId, pageable);
     }
 
     @GetMapping("/search/get-product-by-name-or-code/{input}")
-    public Response<List<Product>> getProductByNameOrCode(@PathVariable String input) {
-        return searchService.getProductByNameOrCode(input);
+    public Response<Page<Product>> getProductByNameOrCode(@PathVariable String input, Pageable pageable) {
+        return searchProductService.getProductByNameOrCode(input, pageable);
     }
 
     @GetMapping("/search/get-top-product")
-    public Response<List<Product>> getTopProduct() {
-        return searchService.getTopProduct();
+    public Response<Page<Product>> getTopProduct(Pageable pageable) {
+        return searchProductService.getTopProduct(pageable);
     }
 
     @GetMapping("/search/search-receipt-online")
-    public Response<List<ReceiptOnline>> searchReceiptOnline(@RequestBody ReceiptSearch searchInfo) {
-        return searchService.searchReceiptOnline(searchInfo);
+    public Response<Page<ReceiptOnline>> searchReceiptOnline(
+            @RequestParam(value = "code", required = false) String code,
+            @RequestParam(value = "status", required = false) Long status,
+            @RequestParam(value = "fromDate", required = false) Date fromDate,
+            @RequestParam(value = "toDate", required = false) Date toDate,
+            Pageable pageable) {
+        return searchReceiptOnlineService.searchReceiptOnline(code, status, fromDate, toDate, pageable);
     }
 }
