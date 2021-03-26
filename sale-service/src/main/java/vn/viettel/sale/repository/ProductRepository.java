960c79a01ec62721fe40706d9e98a2238539789b
Vang Huynh <vanghm@imt-soft.com>
2021-03-26 15:46:33 +0700

    fixed bugs sale service

--- sale-service/src/main/java/vn/viettel/sale/repository/ProductRepository.java
+++ sale-service/src/main/java/vn/viettel/sale/repository/ProductRepository.java
@@ -1,8 +1,7 @@
 package vn.viettel.sale.repository;
 
 import vn.viettel.core.db.entity.common.Product;
-import vn.viettel.core.db.entity.common.Shop;
-import vn.viettel.core.respository.BaseRepository;
+import vn.viettel.core.repository.BaseRepository;
 
 public interface ProductRepository extends BaseRepository<Product> {
 }
--- sale-service/src/main/java/vn/viettel/sale/service/feign/CustomerClient.java
+++ sale-service/src/main/java/vn/viettel/sale/service/feign/CustomerClient.java
@@ -4,9 +4,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import vn.viettel.core.db.entity.common.Customer;
-import vn.viettel.core.messaging.Response;
 import vn.viettel.core.security.anotation.FeignClientAuthenticate;
-import vn.viettel.customer.service.dto.CustomerDTO;
 
 @Service
 @FeignClientAuthenticate(name = "customer-service")
