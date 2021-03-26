e05d97a222f561e1f9267943f4b2e92e28356941
Minh Le <minhld@imt-soft.com>
2021-03-26 14:37:36 +0700

    fix create customer

---
+++ customer-service/src/main/java/vn/viettel/customer/repository/CustomerTypeRepository.java
@@ -0,0 +1,9 @@
+package vn.viettel.customer.repository;
+import vn.viettel.core.db.entity.common.CustomerType;
+import vn.viettel.core.repository.BaseRepository;
+
+import java.util.Optional;
+
+public interface CustomerTypeRepository extends BaseRepository<CustomerType> {
+    Optional<CustomerType> findById(Long id);
+}
