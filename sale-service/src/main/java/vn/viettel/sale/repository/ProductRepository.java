d06385c5c7591d7a7e0fd738f0de6edbd574fdb6
Son Pham <sonpht@imt-soft.com>
2021-03-26 12:03:56 +0700

    api sale order

--- sale-service/src/main/java/vn/viettel/sale/repository/ProductRepository.java
+++ sale-service/src/main/java/vn/viettel/sale/repository/ProductRepository.java
@@ -5,13 +5,12 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
-import vn.viettel.core.db.entity.Product;
 import vn.viettel.core.db.entity.common.Product;
 import vn.viettel.core.repository.BaseRepository;
 
 @Repository
 public interface ProductRepository extends BaseRepository<Product>, JpaSpecificationExecutor<Product> {
-    Page<Product> findByProductTypeId(long id, Pageable pageable);
+//    Page<Product> findByProductTypeId(long id, Pageable pageable);
 
     @Query(value = "SELECT * FROM PRODUCTS WHERE PRODUCT_NAME like %:name%", nativeQuery = true)
     Page<Product> findByProductName(String name, Pageable pageable);
