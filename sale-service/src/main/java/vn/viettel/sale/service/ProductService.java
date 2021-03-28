b27d7f3d6ab94d3a4818dd6f0d74424fc07add1a
Thai Trinh <thaitt@imt-soft.com>
2021-03-28 22:10:07 +0700

    fix code

--- sale-service/src/main/java/vn/viettel/sale/service/ProductService.java
+++ sale-service/src/main/java/vn/viettel/sale/service/ProductService.java
@@ -5,9 +5,10 @@ import org.springframework.data.domain.Pageable;
 import vn.viettel.core.db.entity.common.Product;
 import vn.viettel.core.db.entity.common.ProductInfo;
 import vn.viettel.core.messaging.Response;
+import vn.viettel.core.service.BaseService;
 import vn.viettel.sale.service.dto.ProductDTO;
 
-public interface ProductService {
+public interface ProductService extends BaseService {
     Response<Page<ProductInfo>> findAllProductInfo(Integer status, Integer type, Pageable pageable);
     Response<Page<ProductDTO>> findProductByProductInfo(Long productInfoId, Long customerTypeId, Integer status, Pageable pageable);
     Response<Page<ProductDTO>> findProductsByNameOrCode(String keyWord, Long customerTypeId, Integer status, Pageable pageable);
