64057fa4ef56041f723dc19622f8ff78edf05940
Thai Trinh <thaitt@imt-soft.com>
2021-03-30 10:42:32 +0700

    add feign promotion for sale

--- sale-service/src/main/java/vn/viettel/sale/service/feign/PromotionClient.java
+++ sale-service/src/main/java/vn/viettel/sale/service/feign/PromotionClient.java
@@ -5,6 +5,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import vn.viettel.core.db.entity.promotion.PromotionProgram;
 import vn.viettel.core.db.entity.promotion.PromotionProgramDiscount;
+import vn.viettel.core.db.entity.voucher.Voucher;
+import vn.viettel.core.db.entity.voucher.VoucherSaleProduct;
 import vn.viettel.core.messaging.Response;
 import vn.viettel.core.security.anotation.FeignClientAuthenticate;
 
@@ -17,4 +19,8 @@ public interface PromotionClient {
     Response<List<PromotionProgramDiscount>> listPromotionProgramDiscountByOrderNumber(@PathVariable(name = "orderNumber") String orderNumber);
     @GetMapping("api/promotion/promotion-program-by-id/{id}")
     Response<PromotionProgram> getById(@PathVariable(name = "id") Long id);
+    @GetMapping("api/voucher/get-by-id/{id}")
+    Response<Voucher> getVouchers(@PathVariable Long id);
+    @GetMapping("api/voucher/voucher-sale_product/{voucherProgramId}")
+    Response<List<VoucherSaleProduct>> findVoucherSaleProducts(@PathVariable Long voucherProgramId);
 }
