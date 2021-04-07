49fc24793b284a0ab238c1b61a1690bb34b4e502
Minh Le <minhld@imt-soft.com>
2021-04-07 17:15:09 +0700

    fix api search redInvoice with request body

--- sale-service/src/main/java/vn/viettel/sale/controller/RedInvoiceController.java
+++ sale-service/src/main/java/vn/viettel/sale/controller/RedInvoiceController.java
@@ -3,15 +3,13 @@ package vn.viettel.sale.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import vn.viettel.core.controller.BaseController;
 import vn.viettel.core.db.entity.common.ProductInfo;
 import vn.viettel.core.db.entity.sale.RedInvoice;
 import vn.viettel.core.messaging.Response;
 import vn.viettel.core.security.anotation.RoleAdmin;
+import vn.viettel.sale.messaging.RedInvoiceFilter;
 import vn.viettel.sale.service.RedInvoiceService;
 import vn.viettel.sale.service.dto.RedInvoiceDTO;
 
@@ -25,11 +23,7 @@ public class RedInvoiceController extends BaseController {
 
 //    @RoleAdmin
     @GetMapping("/red-invoices")
-    public Response<Page<RedInvoiceDTO>> findALlProductInfo(@RequestParam(name = "searchKeywords", required = false) String searchKeywords,
-                                                            @RequestParam(name = "fromDate", required = false) Date fromDate,
-                                                            @RequestParam(name = "toDate", required = false) Date toDate,
-                                                            @RequestParam(name = "invoiceNumber", required = false) String invoiceNumber,
-                                                            Pageable pageable) {
-        return redInvoiceService.getAll(searchKeywords, fromDate, toDate, invoiceNumber, pageable);
+    public Response<Page<RedInvoiceDTO>> findALlProductInfo(@RequestBody RedInvoiceFilter redInvoiceFilter, Pageable pageable) {
+        return redInvoiceService.getAll(redInvoiceFilter, pageable);
     }
 }
