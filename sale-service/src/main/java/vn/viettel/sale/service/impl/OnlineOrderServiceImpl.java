b9f4635448d1dd06e860346c1cd097a0e1bfd0dd
Thai Trinh <thaitt@imt-soft.com>
2021-04-08 09:39:16 +0700

    fix bugs

--- sale-service/src/main/java/vn/viettel/sale/service/impl/OnlineOrderServiceImpl.java
+++ sale-service/src/main/java/vn/viettel/sale/service/impl/OnlineOrderServiceImpl.java
@@ -43,6 +43,7 @@ public class OnlineOrderServiceImpl extends BaseServiceImpl<OnlineOrder, OnlineO
     private OnlineOrderDTO mapOnlineOrderToOnlineOrderDTO(OnlineOrder order) {
         modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
         OnlineOrderDTO dto = modelMapper.map(order, OnlineOrderDTO.class);
+        dto.setOrderInfo(order.getCustomerName() + " - " + order.getCustomerPhone());
         return dto;
     }
 }
