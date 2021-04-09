7a9a23b7a7a0b7aac1f8a76aa407fa62ab123e13
Son Pham <sonpht@imt-soft.com>
2021-04-09 09:08:36 +0700

    remake API url

--- sale-service/src/main/java/vn/viettel/sale/service/impl/SaleOrderServiceImpl.java
+++ sale-service/src/main/java/vn/viettel/sale/service/impl/SaleOrderServiceImpl.java
@@ -7,7 +7,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import vn.viettel.core.ResponseMessage;
 import vn.viettel.core.db.entity.authorization.User;
-import vn.viettel.core.db.entity.common.Customer;
 import vn.viettel.core.db.entity.common.Product;
 import vn.viettel.core.db.entity.promotion.PromotionProgram;
 import vn.viettel.core.db.entity.promotion.PromotionProgramDiscount;
@@ -218,61 +217,4 @@ public class SaleOrderServiceImpl implements SaleOrderService {
             }
         return promotionDTOList;
     }
-
-    @Override
-    public Response<Page<SaleOrderDTO>> getAllBillOfSaleList(SaleOrderFilter filter, Pageable pageable) {
-        String customerName, customerCode, companyName, companyAddress, taxCode;
-        Response<Page<SaleOrderDTO>> response = new Response<>();
-        List<SaleOrderDTO> saleOrdersList = new ArrayList<>();
-        List<SaleOrder> saleOrders = saleOrderRepository.findAll(Specification.where(
-                SaleOderSpecification.hasCustomerName(filter.getCustomerName())
-                        .and(SaleOderSpecification.hasOrderNumber(filter.getOrderNumber()))
-                        .and(SaleOderSpecification.hasFromDateToDate(filter.getFromDate(),filter.getToDate()))));
- //       Page<SaleOrderDTO> saleOrderDTOS = saleOrders.map(saleOrder -> this.mapSaleOderToSaleOderDTO(saleOrder));
-        CustomerDTO customer;
-        for(SaleOrder so: saleOrders) {
-            try {
-                customer = customerClient.getCustomerById(so.getCustomerId()).getData();
-            }catch (Exception e) {
-                response.setFailure(ResponseMessage.CUSTOMER_NOT_EXIST);
-                return response;
-            }
-            customerName = customer.getLastName() +" "+ customer.getFirstName();
-            customerCode = customer.getCustomerCode();
-            taxCode = customer.getTaxCode();
-            companyName = customer.getWorkingOffice();
-            companyAddress = customer.getOfficeAddress();
-
-            SaleOrderDTO saleOrder = new SaleOrderDTO();
-            saleOrder.setId(so.getId()); //soId
-            saleOrder.setOrderNumber(so.getOrderNumber()); //soNumber
-            saleOrder.setCustomerId(so.getCustomerId()); //cusId;
-            saleOrder.setCustomerNumber(customerCode);
-            saleOrder.setCustomerName(customerName);
-            saleOrder.setOrderDate(so.getOrderDate());
-
-            saleOrder.setAmount(so.getAmount());
-            saleOrder.setDiscount(so.getTotalPromotion());
-            saleOrder.setAccumulation(so.getCustomerPurchase());
-            saleOrder.setTotal(so.getTotal());
-
-            saleOrder.setNote(so.getNote());
-            saleOrder.setRedReceipt(so.getUsedRedInvoice());
-            saleOrder.setComName(companyName);
-            saleOrder.setTaxCode(taxCode);
-            saleOrder.setAddress(companyAddress);
-            saleOrder.setNoteRed(so.getRedInvoiceRemark());
-            saleOrdersList.add(saleOrder);
-        }
-        Page<SaleOrderDTO> saleOrderResponse = new PageImpl<>(saleOrdersList);
-        response.setData(saleOrderResponse);
-        return response;
-    }
-
-    private SaleOrderDTO mapSaleOderToSaleOderDTO(SaleOrder saleOrder) {
-        modelMapper.getConfiguration().setMatchingStrategy(MatchingStrategies.STRICT);
-        SaleOrderDTO dto = modelMapper.map(saleOrder, SaleOrderDTO.class);
-        return dto;
-    }
-
 }
