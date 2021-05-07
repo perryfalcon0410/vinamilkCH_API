c0af730e1f82a6f09a29433af91793b678603cac
Vang Huynh <vanghm@imt-soft.com>
2021-05-07 09:34:37 +0700

    fix bugs

--- report-service/src/main/java/vn/viettel/report/controller/ShopImportReportController.java
+++ report-service/src/main/java/vn/viettel/report/controller/ShopImportReportController.java
@@ -1,24 +1,14 @@
 package vn.viettel.report.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.io.InputStreamResource;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.HttpHeaders;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import vn.viettel.core.controller.BaseController;
-import vn.viettel.core.messaging.CoverResponse;
 import vn.viettel.core.messaging.Response;
-import vn.viettel.core.security.anotation.RoleAdmin;
-import vn.viettel.report.excel.ShopImportReport;
 import vn.viettel.report.messaging.ShopImportFilter;
 import vn.viettel.report.service.ShopImportReportService;
 import vn.viettel.report.service.dto.ShopImportDTO;
-
-import java.io.ByteArrayInputStream;
-import java.io.IOException;
-import java.util.List;
 
 @RestController
 @RequestMapping("api/report")
