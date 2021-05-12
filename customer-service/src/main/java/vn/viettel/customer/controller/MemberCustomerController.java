313f0194cd3418c18b3a716b4e80dda20c45dc86
Minh Le <minhld@imt-soft.com>
2021-05-12 11:49:20 +0700

    add log

--- customer-service/src/main/java/vn/viettel/customer/controller/MemberCustomerController.java
+++ customer-service/src/main/java/vn/viettel/customer/controller/MemberCustomerController.java
@@ -1,13 +1,20 @@
 package vn.viettel.customer.controller;
 
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import vn.viettel.core.controller.BaseController;
 import vn.viettel.core.dto.customer.MemberCustomerDTO;
+import vn.viettel.core.logging.LogFile;
+import vn.viettel.core.logging.LogLevel;
+import vn.viettel.core.logging.LogMessage;
 import vn.viettel.core.messaging.Response;
 import vn.viettel.customer.entities.MemberCustomer;
 import vn.viettel.customer.service.MemberCustomerService;
 
+import javax.servlet.http.HttpServletRequest;
 import javax.validation.Valid;
 
 @RestController
@@ -16,8 +23,14 @@ public class MemberCustomerController extends BaseController {
     MemberCustomerService memberCustomerService;
     private final String root = "/customers/membercustomers";
 
+    @ApiOperation(value = "Danh sách điểm của thành viên")
+    @ApiResponses(value = {@ApiResponse(code = 200, message = "Success"),
+            @ApiResponse(code = 400, message = "Bad request"),
+            @ApiResponse(code = 500, message = "Internal server error")}
+    )
     @PostMapping(value = { V1 + root})
-    public Response<MemberCustomer> create(@Valid @RequestBody MemberCustomerDTO request) {
+    public Response<MemberCustomer> create(HttpServletRequest httpRequest, @Valid @RequestBody MemberCustomerDTO request) {
+        LogFile.logToFile(appName, getUserName(), LogLevel.INFO, httpRequest, LogMessage.LOGIN_SUCCESS);
         return memberCustomerService.create(request, this.getUserId());
     }
     @GetMapping(value = { V1 + root + "/{id}"})
