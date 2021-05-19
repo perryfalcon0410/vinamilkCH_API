558b728e870c86a4bde807d1f8e8809c1a41d09d
Minh Le <minhld@imt-soft.com>
2021-05-19 14:40:27 +0700

    update code

--- report-service/src/main/java/vn/viettel/report/service/dto/ReportVoucherDTO.java
+++ report-service/src/main/java/vn/viettel/report/service/dto/ReportVoucherDTO.java
@@ -5,10 +5,15 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
+import javax.persistence.Id;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ReportVoucherDTO {
-
+    @Id
+    @Column(name = "ID")
+    private Long id;
 }
