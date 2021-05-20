aeead583508f8734098b5e1cc8b9d0efe45b1612
Minh Le <minhld@imt-soft.com>
2021-05-20 11:46:30 +0700

    api index report voucher

--- report-service/src/main/java/vn/viettel/report/service/dto/ReportVoucherDTO.java
+++ report-service/src/main/java/vn/viettel/report/service/dto/ReportVoucherDTO.java
@@ -6,14 +6,51 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.Column;
+import javax.persistence.Entity;
 import javax.persistence.Id;
+import java.util.Date;
 
 @Getter
 @Setter
-@AllArgsConstructor
 @NoArgsConstructor
+@Entity
 public class ReportVoucherDTO {
     @Id
     @Column(name = "ID")
     private Long id;
+    @Column(name = "SHOP_CODE")
+    private String shopCode;
+    @Column(name = "SHOP_NAME")
+    private String shopName;
+    @Column(name = "VOUCHER_PROGRAM_NAME")
+    private String voucherProgramName;
+    @Column(name = "VOUCHER_CODE")
+    private String voucherCode;
+    @Column(name = "VOUCHER_NAME")
+    private String voucherName;
+    @Column(name = "SERIAL")
+    private String serial;
+    @Column(name = "PRICE")
+    private Float price;
+    @Column(name = "CHANGE_USER")
+    private String changeUser;
+    @Column(name = "CHANGE_DATE")
+    private Date changeDate;
+    @Column(name = "STATUS")
+    private String status;
+    @Column(name = "ACTIVATED")
+    private String activated;
+    @Column(name = "IS_USED")
+    private String isUsed;
+    @Column(name = "CUSTOMER_CODE")
+    private String customerCode;
+    @Column(name = "ORDER_NUMBER")
+    private String orderNumber;
+    @Column(name = "ORDER_SHOP_CODE")
+    private String orderShopCode;
+    @Column(name = "ORDER_DATE")
+    private Date orderDate;
+    @Column(name = "ORDER_AMOUNT")
+    private Float orderAmount;
+
 }
--- report-service/src/main/java/vn/viettel/report/service/impl/ReportVoucherServiceImpl.java
+++ report-service/src/main/java/vn/viettel/report/service/impl/ReportVoucherServiceImpl.java
@@ -1,26 +1,83 @@
 package vn.viettel.report.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import vn.viettel.core.messaging.Response;
 import vn.viettel.report.messaging.ReportVoucherFilter;
 import vn.viettel.report.service.ReportVoucherService;
 import vn.viettel.report.service.dto.ReportVoucherDTO;
+import vn.viettel.report.service.feign.ShopClient;
 
+import javax.persistence.EntityManager;
+import javax.persistence.ParameterMode;
+import javax.persistence.PersistenceContext;
+import javax.persistence.StoredProcedureQuery;
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 @Service
 public class ReportVoucherServiceImpl implements ReportVoucherService {
 
+    @PersistenceContext
+    EntityManager entityManager;
+
+    @Autowired
+    ShopClient shopClient;
+
     @Override
     public Response<Page<ReportVoucherDTO>> index(ReportVoucherFilter filter, Pageable pageable) {
-        return null;
+        StoredProcedureQuery storedProcedure = callReportVoucherDTO(filter);
+        List<ReportVoucherDTO> lst = storedProcedure.getResultList();
+        List<ReportVoucherDTO> subList = new ArrayList<>();
+
+        if(!lst.isEmpty())
+        {
+            Integer l = lst.size();
+            int start = (int)pageable.getOffset();
+            int end = Math.min((start + pageable.getPageSize()), lst.size());
+            subList = lst.subList(start, end);
+        }
+
+        Page<ReportVoucherDTO>  page = new PageImpl<>(subList, pageable, lst.size());
+        return new Response<Page<ReportVoucherDTO>>().withData(page);
     }
 
     @Override
     public ByteArrayInputStream exportExcel(ReportVoucherFilter filter) throws IOException {
         return null;
+    }
+
+    public StoredProcedureQuery callReportVoucherDTO(ReportVoucherFilter filter)
+    {
+        StoredProcedureQuery storedProcedure =
+                entityManager.createStoredProcedureQuery("P_VOUCHER", ReportVoucherDTO.class);
+        storedProcedure.registerStoredProcedureParameter(1, void.class, ParameterMode.REF_CURSOR);
+        storedProcedure.registerStoredProcedureParameter(2, Date.class, ParameterMode.IN);
+        storedProcedure.registerStoredProcedureParameter(3, Date.class, ParameterMode.IN);
+        storedProcedure.registerStoredProcedureParameter(4, Date.class, ParameterMode.IN);
+        storedProcedure.registerStoredProcedureParameter(5, Date.class, ParameterMode.IN);
+        storedProcedure.registerStoredProcedureParameter(6, String.class, ParameterMode.IN);
+        storedProcedure.registerStoredProcedureParameter(7, String.class, ParameterMode.IN);
+        storedProcedure.registerStoredProcedureParameter(8, String.class, ParameterMode.IN);
+        storedProcedure.registerStoredProcedureParameter(9, String.class, ParameterMode.IN);
+        storedProcedure.registerStoredProcedureParameter(10, Long.class, ParameterMode.IN);
+
+        storedProcedure.setParameter(2, filter.getFromProgramDate());
+        storedProcedure.setParameter(3, filter.getToProgramDate());
+        storedProcedure.setParameter(4, filter.getFromUseDate());
+        storedProcedure.setParameter(5, filter.getToUseDate());
+        storedProcedure.setParameter(6, filter.getVoucherProgramName());
+        storedProcedure.setParameter(7, filter.getVoucherKeywords());
+        storedProcedure.setParameter(8, filter.getCustomerKeywords());
+        storedProcedure.setParameter(9, filter.getCustomerMobiPhone());
+        storedProcedure.setParameter(10, filter.getShopId());
+        storedProcedure.execute();
+        return storedProcedure;
     }
 }
