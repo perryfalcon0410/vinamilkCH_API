b5dc241716b0049e090bf18533ebf02ca5cd74d5
Son Pham <sonpht@imt-soft.com>
2021-05-24 17:29:10 +0700

    apidoc sale order return

--- sale-service/src/main/java/vn/viettel/sale/service/dto/PromotionReturnDTO.java
+++ sale-service/src/main/java/vn/viettel/sale/service/dto/PromotionReturnDTO.java
@@ -1,5 +1,6 @@
 package vn.viettel.sale.service.dto;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
@@ -8,10 +9,16 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 public class PromotionReturnDTO {
+    @ApiModelProperty(notes = "Mã sản phẩm")
     private String productCode;
+    @ApiModelProperty(notes = "Tên sản phẩm")
     private String productName;
+    @ApiModelProperty(notes = "Đơn vị")
     private String unit;
+    @ApiModelProperty(notes = "Số lượng")
     private int quantity;
+    @ApiModelProperty(notes = "Giá mỗi đơn vị")
     private float pricePerUnit;
+    @ApiModelProperty(notes = "Tiền phải trả")
     private float paymentReturn;
 }
