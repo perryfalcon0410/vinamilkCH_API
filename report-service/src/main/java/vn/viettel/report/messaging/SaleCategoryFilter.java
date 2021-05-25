869686ac738a6744a31c53d8919c6144b871b2da
sonpht <sonpht@imt-soft.com>
2021-05-26 01:24:33 +0700

    export excel sale category

---
+++ report-service/src/main/java/vn/viettel/report/messaging/SaleCategoryFilter.java
@@ -0,0 +1,29 @@
+package vn.viettel.report.messaging;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class SaleCategoryFilter {
+    private String customerKW;
+    private Date fromDate;
+    private Date toDate;
+    private Long shopId;
+
+    public Date getFromDate() {
+        if(fromDate == null) return new Date();
+        return fromDate;
+    }
+
+    public Date getToDate() {
+        if(toDate == null) return new Date();
+        return toDate;
+    }
+}
