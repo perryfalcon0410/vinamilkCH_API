e1b50db6a22ff78251d0367b2579934224cafd8c
Tu Nguyen <tunguyen2@imt-soft.com>
2021-06-07 17:30:55 +0700

    print redinvoice

---
+++ sale-service/src/main/java/vn/viettel/sale/messaging/PrintDataRedInvoiceResponse.java
@@ -0,0 +1,47 @@
+package vn.viettel.sale.messaging;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import vn.viettel.core.util.Constants;
+
+import java.time.LocalDateTime;
+import java.util.Date;
+
+@Getter
+@Setter
+@NoArgsConstructor
+public class PrintDataRedInvoiceResponse {
+
+    @ApiModelProperty(notes = "Số hóa đơn đỏ")
+    private String redInvoiceNumber;
+    @ApiModelProperty(notes = "Ngày in hóa đơn")
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = Constants.DATE_TIME_PATTERN)
+    private LocalDateTime datePrint;
+    @ApiModelProperty(notes = "Số hóa đơn")
+    private String saleOrderNumber;
+    @ApiModelProperty(notes = "Tên cửa hàng")
+    private String shopName;
+    @ApiModelProperty(notes = "Địa chỉ")
+    private String shopAddress;
+    @ApiModelProperty(notes = "Số điện thoại cửa hàng")
+    private String shopTel;
+    @ApiModelProperty(notes = "Tên khách hàng")
+    private String customerName;
+    @ApiModelProperty(notes = "Địa chỉ")
+    private String customerAddress;
+    @ApiModelProperty(notes = "Số điện thoại")
+    private String customerPhone;
+    @ApiModelProperty(notes = "Thành tiền")
+    private Float amount;
+    @ApiModelProperty(notes = "Thuế GTGT")
+    private Float valueAddedTax;
+    @ApiModelProperty(notes = "Tổng tiền phải trả số")
+    private Float totalAmountNumber;
+    @ApiModelProperty(notes = "Tổng tiền phải trả chữ")
+    private String totalAmountString;
+
+}
+
