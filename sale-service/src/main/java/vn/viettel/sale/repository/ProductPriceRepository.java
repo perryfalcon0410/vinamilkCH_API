7e8135d5d3489b63bcdd83f9d1624182877f6869
Phuong Kim <phuongkt@imt-soft.com>
2021-06-18 19:07:45 +0700

    - fix select in

--- sale-service/src/main/java/vn/viettel/sale/repository/ProductPriceRepository.java
+++ sale-service/src/main/java/vn/viettel/sale/repository/ProductPriceRepository.java
@@ -18,7 +18,7 @@ public interface ProductPriceRepository extends BaseRepository<Price> {
 //    Price getProductPrice(Long productId, Long customerTypeId);
 
     @Query(value = "SELECT p FROM Price p " +
-            "WHERE p.status = 1 AND p.productId IN :productIds AND p.priceType = -1 AND p.customerTypeId =:customerTypeId " +
+            "WHERE p.status = 1 AND p.productId IN (:productIds) AND p.priceType = -1 AND p.customerTypeId =:customerTypeId " +
             "AND ( :date IS NULL OR (p.fromDate IS NULL AND p.toDate IS NULL) OR ( :date BETWEEN p.fromDate AND p.toDate ) " +
             "OR ( p.fromDate <= :date AND p.toDate IS NULL ) OR ( p.fromDate IS NULL AND :date <= p.toDate ) )")
     List<Price> findProductPrice(List<Long> productIds, Long customerTypeId, LocalDateTime date);
