5da25a62e29b499c80d0d4b2fc7f914571597b26
Thai Trinh <thaitt@imt-soft.com>
2021-06-21 18:16:04 +0700

    clean code

--- promotion-service/src/main/java/vn/viettel/promotion/repository/VoucherRepository.java
+++ promotion-service/src/main/java/vn/viettel/promotion/repository/VoucherRepository.java
@@ -11,17 +11,6 @@ import java.util.Optional;
 
 public interface VoucherRepository extends BaseRepository<Voucher>, JpaSpecificationExecutor<Voucher> {
 
-//    @Query( value = "SELECT * FROM VOUCHERS v " +
-//            "JOIN VOUCHER_PROGRAM p ON v.VOUCHER_PROGRAM_ID = p.ID " +
-//            "WHERE p.STATUS = 1 " +
-//            "AND (p.TO_DATE IS NULL OR (p.FROM_DATE <=:fromDate AND p.TO_DATE >=:toDate)) " +
-//            "AND v.SERIAL =:serial " +
-//            "AND v.ACTIVATED = 1 AND v.IS_USED = 0 AND v.STATUS = 1 "
-//            , nativeQuery = true
-//    )
-//    Optional<Voucher> getBySerial(String serial, LocalDateTime fromDate, LocalDateTime toDate);
-
-
     @Query("Select v From Voucher v Join VoucherProgram p On p.id = v.voucherProgramId " +
             "Where p.status = 1 " +
             "And v.serial =:serial AND v.activated = true And v.isUsed = false And v.status = 1 ")
