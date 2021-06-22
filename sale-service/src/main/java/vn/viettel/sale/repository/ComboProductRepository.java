dce3ebf1d806d69d24a3328046bc6add43662dfc
Thai Trinh <thaitt@imt-soft.com>
2021-06-22 15:06:05 +0700

    fix bugs find combo product detail

--- sale-service/src/main/java/vn/viettel/sale/repository/ComboProductRepository.java
+++ sale-service/src/main/java/vn/viettel/sale/repository/ComboProductRepository.java
@@ -2,10 +2,9 @@ package vn.viettel.sale.repository;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.data.jpa.repository.Query;
-import vn.viettel.sale.entities.ComboProduct;
 import vn.viettel.core.repository.BaseRepository;
+import vn.viettel.sale.entities.ComboProduct;
 import vn.viettel.sale.service.dto.ComboProductDetailDTO;
-import vn.viettel.sale.service.dto.FreeProductDTO;
 
 import java.util.List;
 
@@ -16,7 +15,8 @@ public interface ComboProductRepository extends BaseRepository<ComboProduct>, Jp
             "   JOIN ComboProduct cbPro ON cbPro.id = p.comboProductId AND cbPro.status = 1" +
             "   JOIN ComboProductDetail cbDtl ON cbPro.id = cbDtl.comboProductId AND cbDtl.status = 1" +
             "   JOIN Product pDtl ON pDtl.id = cbDtl.productId AND cbDtl.status = 1" +
-            "   JOIN Price price ON price.productId = cbDtl.id AND price.customerTypeId =:customerTypeId AND price.status = 1 AND price.priceType = -1 " +
+            "   JOIN Price price ON price.productId = cbDtl.productId AND price.customerTypeId =:customerTypeId AND price.status = 1 AND price.priceType = -1 " +
             "   WHERE p.id IN :productIds AND p.status = 1  AND p.isCombo = true ")
     List<ComboProductDetailDTO> findComboProduct(Long customerTypeId, List<Long> productIds);
 }
+
