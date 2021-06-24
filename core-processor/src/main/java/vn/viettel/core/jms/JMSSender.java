a1521213122f167b8ee3effe7de56fa57bc76259
lucvt2 <lucvt2@viettel.com.vn>
2021-06-24 10:00:43 +0700

    init dong bo pos sang dms

---
+++ core-processor/src/main/java/vn/viettel/core/jms/JMSSender.java
@@ -0,0 +1,54 @@
+/*
+ * Copyright 2021 Viettel Business Solutions. All rights reserved.
+ * VIETTEL PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+
+package vn.viettel.core.jms;
+
+import java.util.List;
+
+import javax.jms.DeliveryMode;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.springframework.jms.core.JmsTemplate;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class JMSSender {
+
+	private static final String COMMA = ",";
+
+	private static final String DEFAULT_DESTINATION_QUEUE_NAME = "integration_queue";
+
+	private ActiveMQConnectionFactory factory;
+
+	private JmsTemplate jmsTemplate;
+
+	public JMSSender(String username, String password, String url) {
+		try {
+			//password = StringUtil.decryptAES(password);
+			factory = new ActiveMQConnectionFactory(username, password, url);
+			jmsTemplate = new JmsTemplate(factory);
+		} catch (Exception ex) {
+			log.error("khoi tao jmsSender", ex);
+		}
+	}
+
+	public void sendMessage(final String type, List<Long> listId) {
+		StringBuilder sb = new StringBuilder();
+		for (Long id : listId) {
+			if (sb.length() > 0) {
+				sb.append(COMMA);
+			}
+			sb.append(id);
+		}
+		jmsTemplate.convertAndSend(DEFAULT_DESTINATION_QUEUE_NAME, sb.toString(), m -> {
+				m.setJMSType(type);
+				m.setJMSDeliveryMode(DeliveryMode.PERSISTENT);
+				return m;
+		});
+	}
+
+}
---
+++ customer-service/src/main/java/vn/viettel/customer/config/JmsConfig.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2021 Viettel Business Solutions. All rights reserved.
+ * VIETTEL PROPRIETARY/CONFIDENTIAL. Use is subject to license terms.
+ */
+
+
+package vn.viettel.customer.config;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import vn.viettel.core.jms.JMSSender;
+
+@Configuration
+public class JmsConfig {
+
+	@Value("${activemq-url}")
+	private String url;
+
+	@Value("${activemq-username}")
+	private String username;
+
+	@Value("${activemq-password}")
+	private String password;
+
+	@Bean
+	JMSSender jmsSender() {
+		return new JMSSender(username, password, url);
+	}
+
+}
