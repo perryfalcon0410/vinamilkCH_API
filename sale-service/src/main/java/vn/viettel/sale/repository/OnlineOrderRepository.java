c91d007370844ed1f456099c200a25d6c9e9d126
Minh Le <minhld@imt-soft.com>
2021-06-25 11:29:00 +0700

    fixed api sync po and online order

--- sale-service/src/main/java/vn/viettel/sale/repository/OnlineOrderRepository.java
+++ sale-service/src/main/java/vn/viettel/sale/repository/OnlineOrderRepository.java
@@ -1,6 +1,7 @@
 package vn.viettel.sale.repository;
 
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.jpa.repository.Query;
 import vn.viettel.sale.entities.OnlineOrder;
 import vn.viettel.core.repository.BaseRepository;
 
@@ -8,4 +9,7 @@ import java.util.List;
 
 public interface OnlineOrderRepository extends BaseRepository<OnlineOrder>, JpaSpecificationExecutor<OnlineOrder> {
     OnlineOrder findByOrderNumber(String orderNumber);
+
+    @Query(value = "SELECT * FROM ONLINE_ORDER WHERE SYN_STATUS = 1 AND VNM_SYN_STATUS = 0 AND SHOP_ID =:shopId", nativeQuery = true)
+    List<OnlineOrder> findOnlineOrderExportXml(Long shopId);
 }
