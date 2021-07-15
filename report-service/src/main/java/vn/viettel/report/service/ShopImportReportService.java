2193db173677c5e9928ecddffed999f99bb62c8e
Thai Trinh <thaitt@imt-soft.com>
2021-07-15 17:25:08 +0700

    fix bugs print report

--- report-service/src/main/java/vn/viettel/report/service/ShopImportReportService.java
+++ report-service/src/main/java/vn/viettel/report/service/ShopImportReportService.java
@@ -5,8 +5,7 @@ import org.springframework.data.domain.Pageable;
 import vn.viettel.core.messaging.CoverResponse;
 import vn.viettel.core.messaging.Response;
 import vn.viettel.report.messaging.ShopImportFilter;
-import vn.viettel.report.service.dto.PrintShopImportFilterDTO;
-import vn.viettel.report.service.dto.PrintShopImportTotalDTO;
+import vn.viettel.report.service.dto.PrintShopImportDTO;
 import vn.viettel.report.service.dto.ShopImportDTO;
 import vn.viettel.report.service.dto.ShopImportTotalDTO;
 
@@ -17,5 +16,5 @@ public interface ShopImportReportService {
     CoverResponse<Page<ShopImportDTO>, ShopImportTotalDTO> find (ShopImportFilter filter, Pageable pageable);
     Response<List<ShopImportDTO>> callProcedure(ShopImportFilter filter);
     Response<CoverResponse<List<ShopImportDTO>, ShopImportTotalDTO>> dataExcel(ShopImportFilter filter);
-    PrintShopImportFilterDTO print(ShopImportFilter filter, Long shopId);
+    PrintShopImportDTO print(ShopImportFilter filter, Long shopId);
 }
