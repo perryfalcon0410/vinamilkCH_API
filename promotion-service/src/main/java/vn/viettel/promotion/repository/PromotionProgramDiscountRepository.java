4b11ffd77ac406d69ecefb3dcda98146d141b014
Thai Trinh <thaitt@imt-soft.com>
2021-07-16 23:11:54 +0700

    fix bugs MGG

--- promotion-service/src/main/java/vn/viettel/promotion/repository/PromotionProgramDiscountRepository.java
+++ promotion-service/src/main/java/vn/viettel/promotion/repository/PromotionProgramDiscountRepository.java
@@ -27,7 +27,6 @@ public interface PromotionProgramDiscountRepository extends BaseRepository<Promo
             " Join PromotionShopMap sm ON sm.promotionProgramId = pg.id " +
         " WHERE pd.discountCode =:discountCode AND pd.status = 1 AND (pd.isUsed IS NULL OR pd.isUsed = 0) " +
         " AND pg.givenType = 2 AND pg.type = 'ZM' AND pg.status = 1 AND sm.shopId =:shopId AND sm.status = 1" +
-        " AND pg.fromDate <=:lastDay AND ( pg.toDate IS NULL OR pg.toDate >=:firstDay )" +
-        " AND sm.fromDate <=:lastDay AND ( sm.toDate IS NULL OR sm.toDate >=:firstDay )")
+        " AND pg.fromDate <=:lastDay AND ( pg.toDate IS NULL OR pg.toDate >=:firstDay )" )
     Optional<PromotionProgramDiscount> getPromotionProgramDiscount(String discountCode, Long shopId, LocalDateTime firstDay, LocalDateTime lastDay);
 }
