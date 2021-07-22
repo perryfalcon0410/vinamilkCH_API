c645094798fc736d9980474dc4a7c85daa3002d0
Thai Trinh <thaitt@imt-soft.com>
2021-07-22 12:24:30 +0700

    fix bugs uat

--- customer-service/src/main/java/vn/viettel/customer/repository/CustomerTypeRepository.java
+++ customer-service/src/main/java/vn/viettel/customer/repository/CustomerTypeRepository.java
@@ -12,7 +12,8 @@ public interface CustomerTypeRepository extends BaseRepository<CustomerType> {
     Optional<CustomerType> findById(Long id);
 
     @Query(value = "SELECT DISTINCT ct.* FROM CUSTOMER_TYPE ct JOIN CUSTOMERS c ON c.CUSTOMER_TYPE_ID = ct.Id " +
-            " WHERE  c.SHOP_ID =:shopId AND c.IS_DEFAULT = 1 AND ct.STATUS = 1 AND c.STATUS = 1 ", nativeQuery = true)
+            " WHERE  c.SHOP_ID =:shopId AND c.IS_DEFAULT = 1 AND ct.STATUS = 1 AND c.STATUS = 1 " +
+            " ORDER BY ct.UPDATED_AT DESC OFFSET 0 ROWS FETCH NEXT 1 ROWS ONLY ", nativeQuery = true)
     CustomerType getWareHouseTypeIdByShopId(@Param("shopId") Long shopId);
 
     @Query(value = "SELECT * FROM CUSTOMER_TYPE WHERE POS_MODIFY_CUS = 1 AND STATUS = 1 " +
