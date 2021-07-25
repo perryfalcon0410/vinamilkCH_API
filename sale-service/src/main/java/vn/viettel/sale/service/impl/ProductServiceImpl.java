fdf065ae45413ce35e598175e1d6e6dc9ed453ee
Thai Trinh <thaitt@imt-soft.com>
2021-07-25 16:28:47 +0700

    fix bugs search product

--- sale-service/src/main/java/vn/viettel/sale/service/impl/ProductServiceImpl.java
+++ sale-service/src/main/java/vn/viettel/sale/service/impl/ProductServiceImpl.java
@@ -244,8 +244,8 @@ public class ProductServiceImpl extends BaseServiceImpl<Product, ProductReposito
 
     private CustomerTypeDTO getCustomerType(Long shopId, Long customerId){
         CustomerTypeDTO customerType = null;
-        if(customerId != null) customerType = customerTypeClient.getCusTypeByCustomerIdV1(shopId);
-        if(customerType == null) customerType = customerTypeClient.getCusTypeIdByShopIdV1(customerId);
+        if(customerId != null) customerType = customerTypeClient.getCusTypeByCustomerIdV1(customerId);
+        if(customerType == null) customerType = customerTypeClient.getCusTypeIdByShopIdV1(shopId);
         if(customerType == null) throw new ValidateException(ResponseMessage.CUSTOMER_TYPE_NOT_EXISTS);
 
         return customerType;
