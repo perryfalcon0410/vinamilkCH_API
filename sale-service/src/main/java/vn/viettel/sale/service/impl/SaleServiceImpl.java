7568fb7b743ed94748d775aba1d33efac5f1a612
Thai Trinh <thaitt@imt-soft.com>
2021-07-26 23:30:07 +0700

    fix bugs uat

--- sale-service/src/main/java/vn/viettel/sale/service/impl/SaleServiceImpl.java
+++ sale-service/src/main/java/vn/viettel/sale/service/impl/SaleServiceImpl.java
@@ -774,7 +774,6 @@ public class SaleServiceImpl extends BaseServiceImpl<SaleOrder, SaleOrderReposit
 
         String  year = Integer.toString(now.getYear()).substring(2);
 
-      //  int STT = repository.countSaleOrder(start,end,shop.getId()) + 1;
         return  "SAL." +  shop.getShopCode() + year + Integer.toString(month + 100).substring(1)  + Integer.toString(day + 100).substring(1) + Integer.toString(STT + 100000).substring(1);
     }
 
