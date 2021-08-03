6e1facfd4a0c29612ec07208a2592667f8d94259
Phuong Kim <phuongkt@imt-soft.com>
2021-08-03 17:33:31 +0700

    - fix bug sale order return price

--- sale-service/src/main/java/vn/viettel/sale/service/impl/OrderReturnImpl.java
+++ sale-service/src/main/java/vn/viettel/sale/service/impl/OrderReturnImpl.java
@@ -332,9 +332,11 @@ public class OrderReturnImpl extends BaseServiceImpl<SaleOrder, SaleOrderReposit
                 productReturn.setOrderDate(returnDate);
 
                 productReturn.setQuantity(saleOrderDetail.getQuantity() * -1);
-//                productReturn.setPrice(saleOrderDetail.getPrice() * -1);
+                productReturn.setPrice(saleOrderDetail.getPrice());
+                productReturn.setPriceNotVat(saleOrderDetail.getPriceNotVat());
                 productReturn.setAmount(saleOrderDetail.getAmount() * -1);
                 productReturn.setTotal(saleOrderDetail.getTotal() * -1);
+
                 if (saleOrderDetail.getAutoPromotionNotVat() != null)
                     productReturn.setAutoPromotionNotVat(saleOrderDetail.getAutoPromotionNotVat() * -1);
                 if (saleOrderDetail.getAutoPromotionVat() != null)
