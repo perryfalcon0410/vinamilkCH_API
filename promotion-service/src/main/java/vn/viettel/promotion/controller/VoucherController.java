9cb6b23fa4b8275aaa392559aa1bfa07baa68e29
Thai Trinh <thaitt@imt-soft.com>
2021-08-27 21:27:50 +0700

    clean code

--- promotion-service/src/main/java/vn/viettel/promotion/controller/VoucherController.java
+++ promotion-service/src/main/java/vn/viettel/promotion/controller/VoucherController.java
@@ -32,10 +32,10 @@ public class VoucherController extends BaseController {
             @ApiResponse(code = 400, message = "Bad request"),
             @ApiResponse(code = 500, message = "Internal server error")}
     )
-    public Response<VoucherDTO> getVoucherByCode(HttpServletRequest request, @PathVariable Map<String, String> voucher,
+    public Response<VoucherDTO> getVoucherByCode(HttpServletRequest request, @PathVariable String serial,
                                            @ApiParam("Id khách hàng") @RequestParam("customerId") Long customerId,
                                            @ApiParam("Id các  sản phẩm mua") @RequestParam("productIds") List<Long> productIds) {
-        VoucherDTO response = voucherService.getVoucherBySerial(voucher.get("serial"), this.getShopId(), customerId, productIds);
+        VoucherDTO response = voucherService.getVoucherBySerial(serial, this.getShopId(), customerId, productIds);
         LogFile.logToFile(appName, getUserName(), LogLevel.INFO, request, LogMessage.GET_VOUCHER_SUCCESS);
         return new Response<VoucherDTO>().withData(response);
     }
@@ -46,10 +46,10 @@ public class VoucherController extends BaseController {
             @ApiResponse(code = 400, message = "Bad request"),
             @ApiResponse(code = 500, message = "Internal server error")}
     )
-    public Response<VoucherDTO> getVoucherBySerial(HttpServletRequest request, @RequestBody String serial,
+    public Response<VoucherDTO> getVoucherBySerial(HttpServletRequest request, @RequestBody Map<String, String> voucher,
                                                  @ApiParam("Id khách hàng") @RequestParam("customerId") Long customerId,
                                                  @ApiParam("Id các  sản phẩm mua") @RequestParam("productIds") List<Long> productIds) {
-        VoucherDTO response = voucherService.getVoucherBySerial(serial, this.getShopId(), customerId, productIds);
+        VoucherDTO response = voucherService.getVoucherBySerial(voucher.get("serial"), this.getShopId(), customerId, productIds);
         LogFile.logToFile(appName, getUserName(), LogLevel.INFO, request, LogMessage.GET_VOUCHER_SUCCESS);
         return new Response<VoucherDTO>().withData(response);
     }
