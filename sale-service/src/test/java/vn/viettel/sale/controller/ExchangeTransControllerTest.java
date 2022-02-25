44f838423e5d584896205a982230ffb6ca803693
Tuan Le <tuanlq@imt-soft.com>
2022-02-25 13:39:51 +0700

    Sale Unit test

--- sale-service/src/test/java/vn/viettel/sale/controller/ExchangeTransControllerTest.java
+++ sale-service/src/test/java/vn/viettel/sale/controller/ExchangeTransControllerTest.java
@@ -4,8 +4,11 @@ import org.junit.Before;
 import org.junit.Test;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.Mockito;
 import org.mockito.MockitoAnnotations;
 import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.PageRequest;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MvcResult;
@@ -15,15 +18,20 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import vn.viettel.core.dto.common.CategoryDataDTO;
 import vn.viettel.core.messaging.CoverResponse;
 import vn.viettel.core.messaging.Response;
+import vn.viettel.core.util.DateUtils;
 import vn.viettel.sale.BaseTest;
 import vn.viettel.sale.entities.ExchangeTrans;
 import vn.viettel.sale.repository.ExchangeTransRepository;
 import vn.viettel.sale.service.ExchangeTranService;
 import vn.viettel.sale.service.dto.ExchangeTotalDTO;
 import vn.viettel.sale.service.dto.ExchangeTransDTO;
+import vn.viettel.sale.service.feign.CategoryDataClient;
 import vn.viettel.sale.service.impl.ExchangeTranServiceImpl;
 
+import java.text.DateFormat;
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 
 import static org.junit.Assert.assertEquals;
@@ -44,13 +52,31 @@ public class ExchangeTransControllerTest extends BaseTest {
     @Mock
     ExchangeTransRepository repository;
 
+    @Mock
+    CategoryDataClient categoryDataClient;
+
+    private List<ExchangeTrans> exchangeTransList;
+
+    private List<CategoryDataDTO> categoryDataDTOS;
+
     @Before
     public void init() {
         MockitoAnnotations.initMocks(this);
         serviceImp.setModelMapper(this.modelMapper);
         final ExchangeTransController controller = new ExchangeTransController();
         controller.setService(service);
+        this.setupAction(controller);
         mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+
+        exchangeTransList = new ArrayList<>();
+        final ExchangeTrans exchangeTrans = new ExchangeTrans();
+        exchangeTrans.setId(1L);
+        exchangeTransList.add(exchangeTrans);
+
+        categoryDataDTOS = new ArrayList<>();
+        final CategoryDataDTO categoryDataDTO = new CategoryDataDTO();
+        categoryDataDTO.setId(1L);
+        categoryDataDTOS.add(categoryDataDTO);
     }
 
     //-------------------------------getAllReason-------------------------------
@@ -58,34 +84,38 @@ public class ExchangeTransControllerTest extends BaseTest {
     public void getAllReasonTest() throws Exception {
         String url = uri + "/reasons";
 
-        Response<List<CategoryDataDTO>> result = new Response<>();
-        List<CategoryDataDTO> data = new ArrayList<>();
-        data.add(new CategoryDataDTO());
-
-        result.setData(data);
-
-        given(service.getReasons()).willReturn(result);
+        Response<List<CategoryDataDTO>> result = serviceImp.getReasons();
 
         ResultActions resultActions = mockMvc.perform(get(url)
-                .contentType(MediaType.APPLICATION_JSON))
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print());
 
-        MvcResult mvcResult = resultActions.andReturn();
-        assertEquals(200, mvcResult.getResponse().getStatus());
+        assertEquals(200, resultActions.andReturn().getResponse().getStatus());
     }
 
     //-------------------------------getAllExchangeTrans-------------------------------
     @Test
     public void getAllExchangeTransTest() throws Exception {
+
+        Date fromDate = new Date("2022/02/22");
+        Date toDate = new Date("2022/02/22");
+
+        Mockito.when(repository.getExchangeTrans(1L, "A", 1, 1L, DateUtils.convertFromDate(fromDate), DateUtils.convertToDate(toDate), PageRequest.of(1,5)))
+                .thenReturn(new PageImpl<>(exchangeTransList));
+
+        Mockito.when(categoryDataClient.getReasonExchangeFeign(Arrays.asList(-1L), null, null))
+                .thenReturn(new Response<List<CategoryDataDTO>>().withData(categoryDataDTOS));
+
         CoverResponse<Page<ExchangeTransDTO>, ExchangeTotalDTO> result =
-                service.getAllExchange(any(), any(), any(), any(), any(), any());
+                serviceImp.getAllExchange(1L, "A", fromDate, toDate, 1L, PageRequest.of(1,5));
 
         ResultActions resultActions = mockMvc.perform(get(uri)
-                .contentType(MediaType.APPLICATION_JSON))
+                        .param("fromDate", "2022/02/22")
+                        .param("toDate", "2022/02/22")
+                        .contentType(MediaType.APPLICATION_JSON))
                 .andDo(MockMvcResultHandlers.print());
 
-        MvcResult mvcResult = resultActions.andReturn();
-        assertEquals(200, mvcResult.getResponse().getStatus());
+        assertEquals(200, resultActions.andReturn().getResponse().getStatus());
     }
 
     //-------------------------------create-------------------------------
